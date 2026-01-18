1a6928fa17bb7e6061a6d8ffceca3513d7fa6fd8
Darksp33d <durrani.saeed1@gmail.com>
2026-01-17 23:12:14 -0500

    Initial Commit

---
+++ MyAmanah/MyAmanahApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAmanahApp.swift
+//  MyAmanah
+//
+//  Created by Saeed Durrani on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct MyAmanahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
